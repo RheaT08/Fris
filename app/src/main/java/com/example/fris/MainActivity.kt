50adee1766a00514e6d16d3c57904fa0fb2d2ba2
RheaM <rheamarie-@live.no>
2021-05-04 14:47:31 +0200

    fragments-implemented

--- app/src/main/java/com/example/fris/MainActivity.kt
+++ app/src/main/java/com/example/fris/MainActivity.kt
@@ -1,11 +1,34 @@
 package com.example.fris
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+
+//PRIMÆR TEKNISKE KRAV
+//Minimum 3 fragments - DONE
+//SplashActivity
+//TODO: Fingeravtrykk sensor
+//TODO: Implementasjon av Room DB og lagring av minst 1 Entitet
+//TODO: Ikke hardkodet farger, tekster, og størrelser.
+
+//Tasks
+//TODO: Sett opp Menu page
+//TODO: Sett opp Bestillings flyt
+
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+        navView.setupWithNavController(navController)
     }
 }
--- app/src/main/java/com/example/fris/SplashActivity.kt
+++ app/src/main/java/com/example/fris/SplashActivity.kt
@@ -1,11 +1,43 @@
 package com.example.fris
 
+import android.content.Context
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.WindowManager
 
+@Suppress("DEPRECATION")
 class SplashActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-    }
-}
+
+        //Setter selve splashscreenen i fullskjerm, uten noe titlebar oppe.
+        //TODO: Fikse deprecation problemet senere.
+        window.setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,    // This is used to hide the status bar and make
+            WindowManager.LayoutParams.FLAG_FULLSCREEN     // the splash screen as a full screen activity.
+        )
+
+        Handler(Looper.getMainLooper()).postDelayed({
+
+            //Intent - tar brukeren med til neste aktivitet/fragment utifra om de er logget inn eller ikke.
+            //overfør brukeren videre fra LoginActivity til chatfragment(MainActivity)
+            //sjekker om id-en allerede er i sharedPref. Ved log out, skal den være slettet.
+            val activityIntent = Intent(this, MainActivity::class.java)
+
+
+            startActivity(activityIntent) //viderefører brukeren
+            finish()
+
+        }, 2000)   //2 seconds on splashscreen
+
+    }//onCreate
+}//END
+
+
+
